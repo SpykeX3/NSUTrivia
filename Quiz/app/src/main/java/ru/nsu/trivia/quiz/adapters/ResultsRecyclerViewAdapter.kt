4fe2c95f027e835edaa7d5f9a4d777e9d0c5bcec
katerina <bromkate@mail.ru>
2021-04-24 22:53:05 +0700

    added adapter before killing our project

---
+++ Quiz/app/src/main/java/ru/nsu/trivia/quiz/adapters/ResultsRecyclerViewAdapter.kt
@@ -0,0 +1,43 @@
+package ru.nsu.trivia.quiz.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ru.nsu.trivia.common.dto.model.PlayerInLobby
+import ru.nsu.trivia.quiz.QuizActivity
+import ru.nsu.trivia.quiz.R
+
+class ResultsRecyclerViewAdapter(var context: Context, var responseList: List<PlayerInLobby>) :
+    RecyclerView.Adapter<ResultsRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.layout_result, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val request = responseList[position]
+        holder.textView.text = request.username
+        holder.textView.setOnClickListener{ view ->
+            (context as QuizActivity).showCorrect(view)
+        }
+        holder.imageView.visibility = if (request.isHost){
+            View.VISIBLE
+        } else{
+            View.INVISIBLE
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return responseList.size
+    }
+
+    class ViewHolder internal constructor(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.player_name)
+        val imageView: ImageView = view.findViewById(R.id.ready_image)
+    }
+}
