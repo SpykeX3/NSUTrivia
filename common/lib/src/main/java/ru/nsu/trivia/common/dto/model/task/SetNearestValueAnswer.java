8c40f071e9ea21350d3b64046ecfcd8bb8890916
SpykeX3 <spykex3@gmail.com>
2021-05-27 19:17:40 +0700

    add SetNearestValueAnswer

--- common/lib/src/main/java/ru/nsu/trivia/common/dto/model/task/SetNearestValueAnswer.java
+++ common/lib/src/main/java/ru/nsu/trivia/common/dto/model/task/SetNearestValueAnswer.java
@@ -1,5 +1,21 @@
 package ru.nsu.trivia.common.dto.model.task;
 
 public class SetNearestValueAnswer extends Answer {
+    private int answer;
 
+    public SetNearestValueAnswer() {
+    }
+
+    public SetNearestValueAnswer(String token, int round, int answer) {
+        super(token, round);
+        this.answer = answer;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
 }
